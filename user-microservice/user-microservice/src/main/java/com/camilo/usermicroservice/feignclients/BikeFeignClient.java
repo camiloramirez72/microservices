434c1a1842b3b9024881c6bebc3a7deb79fd9e61
Camilo Ramirez <camilo.ramirez@elconsentimiento.es>
2022-05-26 17:15:36 +0200

    multiple

--- user-microservice/user-microservice/src/main/java/com/camilo/usermicroservice/feignclients/BikeFeignClient.java
+++ user-microservice/user-microservice/src/main/java/com/camilo/usermicroservice/feignclients/BikeFeignClient.java
@@ -2,11 +2,12 @@ package com.camilo.usermicroservice.feignclients;
 
 import com.camilo.usermicroservice.model.Bike;
 import com.camilo.usermicroservice.model.Car;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "bike-service", path = "/bike")
+@FeignClient(name = "bike-microservice", path = "/bike")
 public interface BikeFeignClient {
 
     @PostMapping()
